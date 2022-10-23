699cc20a69a5b7b2aa5d775a9491f956fb693554
UnknownMaillet <62292183+arnaudmaillet@users.noreply.github.com>
2022-10-23 15:07:47 +0200

    release: add muzzle flash animation

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    private Animator anim;
+    [Header("Weapon")]
     public Transform shootPoint;
     public float range = 100f;
     public int magazin = 31;
     public float fireRate = 0.5f;
     float fireTimer; // Temps entre les clicks gauche de la souris
-    public int bulletsInMagazin;
-    public int bulletLeft;
+
+    [Header("Animations")]
+    public ParticleSystem muzzleFlash;
+    public ParticleSystem SparksEmitter;
+    public ParticleSystem SparksEmitter_1;
+
+    [Header("Sound")]
+    public AudioClip shootSound;
+
+    private int _BulletsInMagazin;
+    private Animator anim;
+    private AudioSource _AudioSource;
 
     void Start()
     {
         anim = GetComponent<Animator>();
-        bulletsInMagazin = magazin;
+        _AudioSource = GetComponent<AudioSource>();
+        _BulletsInMagazin = magazin;
     }
 
     void Update()
@@ -29,19 +40,28 @@ public class Weapon : MonoBehaviour
     void FixedUpdate()
     {
         AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(0);
-        if (info.IsName("Fire")) anim.SetBool("Fire", false);
     }
 
     public void fire()
     {
-        if (fireTimer < fireRate) return;
+        if (fireTimer < fireRate || magazin <= 0) return;
         RaycastHit hit;
         if (Physics.Raycast(shootPoint.position, shootPoint.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name + " hit");
         }
-        anim.CrossFade("Fire", 0.01f); //assigner true au parametre Fire de l'animator --> Déclenche l'animation
-        bulletsInMagazin--;
+        anim.CrossFadeInFixedTime("Fire", 0.01f); //assigner true au parametre Fire de l'animator --> Déclenche l'animation
+        muzzleFlash.Play();
+        SparksEmitter.Play();
+        SparksEmitter_1.Play();
+        PlayShootSound();
+        _BulletsInMagazin--;
         fireTimer = 0.0f;   
+    }
+
+    private void PlayShootSound()
+    {
+        _AudioSource.clip = shootSound;
+        _AudioSource.Play();
     }
 }
